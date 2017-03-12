872b2061fb305d526beb9ac49357a157d6bb0db8
Faria <fariashen@gmail.com>
2017-03-12 09:57:06 +0800

    整合Spring和Hibernate，成功运行生成数据表

---
+++ EatCake/src/eatcake/model/User.java
@@ -0,0 +1,21 @@
+package eatcake.model;
+
+public class User {
+	private Integer id;
+	private String userName;
+	private String passWord;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	
+	
+}
